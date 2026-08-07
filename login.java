eacf2d7acc89cfdec0e030ab891b4c521b25278e
Bhavana <dostibhavanareddy@gmail.com>
2026-08-07 11:33:23 +0000

    java file

---
+++ login.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        // Loop from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
